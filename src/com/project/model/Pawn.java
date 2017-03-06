fb93fc3566dad2d9297dc4209954eaf9f11ca95b
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-03-06 16:22:53 +0400

    cretae white pawn and meke the test pass

---
+++ src/com/project/model/Pawn.java
@@ -0,0 +1,23 @@
+package com.project.model;
+
+/**
+ *
+ * @author teyyub 4:13:52 PM
+ */
+public class Pawn {
+
+    private String color;
+
+    public Pawn() {
+
+    }
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
---
+++ test/PawnTest.java
@@ -0,0 +1,18 @@
+
+import com.project.model.Pawn;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author teyyub 12:33:51 PM
+ */
+public class PawnTest {
+
+    @Test
+    public void testCreate() {
+         Pawn whitePawn = new Pawn("white");
+         String whitePawnColor = "white";
+         assertEquals(whitePawn, whitePawn);
+    }
+}
