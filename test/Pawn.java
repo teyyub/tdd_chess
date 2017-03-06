b51647ba72c9b19177478624bcd0ca3eb66a8453
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-03-06 12:49:25 +0400

    first project initial

---
+++ test/Pawn.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author teyyub 12:33:51 PM
+ */
+public class Pawn {
+ @Test
+  void testCreate(){
+      fail("first method");
+  }
+}
