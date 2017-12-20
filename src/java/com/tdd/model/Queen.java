c6a91191f203aecbeb07df860c70eb5ba3493a5d
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-12-20 12:44:25 +0400

    cleaned some classes

--- src/java/com/tdd/model/Queen.java
+++ src/java/com/tdd/model/Queen.java
@@ -6,32 +6,32 @@ package com.tdd.model;
  */
 public class Queen extends Piece {
 
-    static final String QUEEN = "q";
-    private static final String WHITE_COLOR = "WHITE";
+//    static final String QUEEN = "q";
+//    private static final String WHITE_COLOR = "WHITE";
 //    private static final String BLACK_COLOR = "BLACK";
     private static final int QUEEN_VALUE = 9;
  
     public Queen() {
     }
 
-    public static Queen createQueen(String color) {
-        return new Queen(color);
-    }
+//    public static Queen createQueen(String color) {
+//        return new Queen(color);
+//    }
 
 
     
-    Queen(String color) {
-//        setColor(color);
-//        if (color == null ? WHITE_COLOR == null : color.equals(WHITE_COLOR)) {
-//            setRepresentation(QUEEN);
-//        } else
-//        {
-//            setRepresentation(QUEEN.toUpperCase());
-//        }
-    }
- 
-    public int getValue() {
-        return QUEEN_VALUE;
-    }
+//    Queen(String color) {
+////        setColor(color);
+////        if (color == null ? WHITE_COLOR == null : color.equals(WHITE_COLOR)) {
+////            setRepresentation(QUEEN);
+////        } else
+////        {
+////            setRepresentation(QUEEN.toUpperCase());
+////        }
+//    }
+// 
+//    public int getValue() {
+//        return QUEEN_VALUE;
+//    }
 
 }
