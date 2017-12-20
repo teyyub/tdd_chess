d29cc7c4566445ead38c1b9c422e4ffa1ad66bdb
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-12-20 12:22:27 +0400

    Created a separate factory method on Piece for each color/piece combination (e.g., createWhitePawn, createBlackRook).

--- src/java/com/tdd/model/Piece.java
+++ src/java/com/tdd/model/Piece.java
@@ -4,30 +4,25 @@ package com.tdd.model;
  *
  * @author teyyub Mar 25, 2016 11:29:59 PM
  */
-public class Piece   {
-
-   
+public class Piece {
 
 //    @Override
 //    public boolean isValidMove(Type type) {
 //        throw new UnsupportedOperationException("Not supported yet.");
 //    }
-
-    public enum Colors {
-        WHITE, BLACK, NullColor
-    };
-
-    public enum Type {
-        PAWN, ROOK, BISHOP, KNIGHT, QUEEN, KING, NOPIECE;
-    };
-
+//    public enum Colors {
+//        WHITE, BLACK, NullColor
+//    };
+//
+//    public enum Type {
+//        PAWN, ROOK, BISHOP, KNIGHT, QUEEN, KING, NOPIECE;
+//    };
 //    Map<Piece.Type, Integer> values = null;
- 
     private String color;
     private String representation;
-    private static final String WHITE_COLOR = "WHITE";
-    private static final String BLACK_COLOR = "BLACK";
-    private static final String NULL_COLOR = "Null";
+//    private static final String WHITE_COLOR = "WHITE";
+//    private static final String BLACK_COLOR = "BLACK";
+//    private static final String NULL_COLOR = "Null";
     static final String ROOK = "r";
     static final String KNIGHT = "n";
     static final String BISHOP = "b";
@@ -47,94 +42,117 @@ public class Piece   {
 //        values.put(Type.KNIGHT, 3);
 //        values.put(Type.PAWN, 1);
 //    }
-
 //    public Map<Type, Integer> getValues() {
 //        if (values == null) {
 //            loadValues();
 //        }
 //        return values;
 //    }
-
-    
-    public static Piece createWhiteQueen(){
-        return new Queen(WHITE_COLOR);
-    }
-    
-    
+//    public static Piece createWhiteQueen(){
+//        return new Queen(WHITE_COLOR);
+//    }
     public static Piece createPiece(Colors c, Type p) {
         return new Piece(c, p);
     }
 
-    private Piece(Colors color, Type type) {
-//        if (type == Type.PAWN) {
-//            if (color == Colors.WHITE) {
-//                Pawn.createPawn(WHITE_COLOR);
-//            }
-//        }
+    private static Piece createWhitePawn() {
+        return new Piece(Colors.WHITE, Type.PAWN);
     }
-//    private Piece(Colors c, Type p) {
-//        if (null != c) {
-//            switch (c) {
-//                case WHITE:
-//                    this.color = WHITE_COLOR;
-//                    break;
-//                case BLACK:
-//                    this.color = BLACK_COLOR;
-//                    break;
-//                default:
-//                    this.color = NULL_COLOR;
-//                    break;
-//            }
-//        }
-//
-//        if (p == Type.Pawn) {
-//            if (c == Colors.WHITE) {   
-//                
-//                this.representation = PAWN;
-//            } else {
-//                this.representation = PAWN.toUpperCase();
-//            }
-//        }
-//        if (p == Type.Rook) {
-//            if (c == Colors.WHITE) {
-//                this.representation = ROOK;
-//            } else {
-//                this.representation = ROOK.toUpperCase();
-//            }
-//        }
-//        if (p == Type.Bishop) {
-//            if (c == Colors.WHITE) {
-//                this.representation = BISHOP;
-//            } else {
-//                this.representation = BISHOP.toUpperCase();
-//            }
-//        }
-//        if (p == Type.Knight) {
-//            if (c == Colors.WHITE) {
-//                this.representation = KNIGHT;
-//            } else {
-//                this.representation = KNIGHT.toUpperCase();
-//            }
-//        }
-//        if (p == Type.Queen) {
-//            if (c == Colors.WHITE) {
-//                this.representation = QUEEN;
-//            } else {
-//                this.representation = QUEEN.toUpperCase();
-//            }
-//        }
-//        if (p == Type.King) {
-//            if (c == Colors.WHITE) {
-//                this.representation = KING;
-//            } else {
-//                this.representation = KING.toUpperCase();
-//            }
-//        }
-//        if (p == Type.NoPiece) {
-//            this.representation = ".";
-//        }
-//    }
-   
+
+    private static Piece createWhiteRook() {
+        return new Piece(Colors.WHITE, Type.ROOK);
+    }
+
+    private static Piece createWhiteBishop() {
+        return new Piece(Colors.WHITE, Type.BISHOP);
+    }
+
+    private static Piece createWhiteKnight() {
+        return new Piece(Colors.WHITE, Type.KNIGHT);
+    }
+
+    private static Piece createWhiteQueen() {
+        return new Piece(Colors.WHITE, Type.QUEEN);
+    }
+
+    private static Piece createWhiteKing() {
+        return new Piece(Colors.WHITE, Type.KING);
+    }
+
+    private static Piece createBlackPawn() {
+        return new Piece(Colors.BLACK, Type.PAWN);
+    }
+
+    private static Piece createBlackRook() {
+        return new Piece(Colors.BLACK, Type.ROOK);
+    }
+
+    private static Piece createBlackBishop() {
+        return new Piece(Colors.BLACK, Type.BISHOP);
+    }
+
+    private static Piece createBlackKnight() {
+        return new Piece(Colors.BLACK, Type.KNIGHT);
+    }
+
+    private static Piece createBlackQueen() {
+        return new Piece(Colors.BLACK, Type.QUEEN);
+    }
+
+    private static Piece createBlackKing() {
+        return new Piece(Colors.BLACK, Type.KING);
+    }
+
+    private Piece(Colors c, Type p) {
+
+        if (p == Type.PAWN) {
+            if (c == Colors.WHITE) {
+
+                this.representation = PAWN;
+            } else {
+                this.representation = PAWN.toUpperCase();
+            }
+        }
+        if (p == Type.ROOK) {
+            if (c == Colors.WHITE) {
+                this.representation = ROOK;
+            } else {
+                this.representation = ROOK.toUpperCase();
+            }
+        }
+        if (p == Type.BISHOP) {
+            if (c == Colors.WHITE) {
+                this.representation = BISHOP;
+            } else {
+                this.representation = BISHOP.toUpperCase();
+            }
+        }
+        if (p == Type.KNIGHT) {
+            if (c == Colors.WHITE) {
+                this.representation = KNIGHT;
+            } else {
+                this.representation = KNIGHT.toUpperCase();
+            }
+        }
+        if (p == Type.QUEEN) {
+            if (c == Colors.WHITE) {
+                this.representation = QUEEN;
+            } else {
+                this.representation = QUEEN.toUpperCase();
+            }
+        }
+        if (p == Type.KING) {
+            if (c == Colors.WHITE) {
+                this.representation = KING;
+            } else {
+                this.representation = KING.toUpperCase();
+            }
+        }
+        if (p == Type.NOPIECE) {
+            this.representation = ".";
+        }
+    }
+
     private Piece(String color, Pawn p) {
         this.color = color;
     }
@@ -150,18 +168,17 @@ public class Piece   {
     public String getRepresentation() {
         return representation;
     }
+
     public void setRepresentation(String representation) {
         this.representation = representation;
     }
 
     public boolean isWhite() {
-        return (getColor() == null ? WHITE_COLOR == null : getColor().equals(WHITE_COLOR));
+        return (getColor() == null ? Colors.WHITE == null : getColor().equals(Colors.WHITE));
     }
 
     public boolean isBlack() {
-        return (getColor() == null ? BLACK_COLOR == null : getColor().equals(BLACK_COLOR));
+        return (getColor() == null ? Colors.BLACK == null : getColor().equals(Colors.BLACK));
     }
-
-   
 
 }
